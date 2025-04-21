89244ceddf6e2c9b8a70de8d1203e33aa568f814
zlem8952 <68328391+zlem8952@users.noreply.github.com>
2025-04-21 17:02:17 +0900

    Update apiCache.js

--- medical/apis/utils/apiCache.js
+++ medical/apis/utils/apiCache.js
@@ -1,1 +1,18 @@
+const cache = new Map();
+
+export const cacheManager = {
+  get(key) {
+    const entry = cache.get(key);
+    if (entry && entry.expires > Date.now()) return entry.data;
+    cache.delete(key);
+    return null;
+  },
+
+  set(key, data, ttl = 60000) {
+    cache.set(key, {
+      data,
+      expires: Date.now() + ttl
+    });
+  }
+};
 
